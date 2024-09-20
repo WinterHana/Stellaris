cce6fcd748a5751845950d87ff772a7f5f8661c6
WinterHana <haha920@naver.com>
2024-09-20 23:00:53 +0900

    feat : init project

---
+++ stellaris/src/main/java/org/stellaris/com/stellaris/StellarisApplication.java
@@ -0,0 +1,13 @@
+package org.stellaris.com.stellaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarisApplication.class, args);
+	}
+
+}
---
+++ stellaris/src/test/java/org/stellaris/com/stellaris/StellarisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stellaris.com.stellaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
